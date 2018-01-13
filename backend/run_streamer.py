8b4b8b29d235e21c98bdc246f2d2183d38521396
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2018-01-13 16:14:06 +0100

    run streamer sleeps better

--- backend/run_streamer.py
+++ backend/run_streamer.py
@@ -1,6 +1,9 @@
 """
 Runs the streamer.
 
+This version of the script runs for 24 hours, then sleeps for 48 hours, and resumes. 
+NOTE: It only checks the time when exceptions are raised though. Otherwise I would need to run async.
+
 @author: Ludvig Olsen
 """
 
@@ -15,7 +18,9 @@ from backend.email_me_error import email_me_error
 __all__ = ["run_streamer"]
 
 
-def run_streamer(tw_connection, listener, WORDS, LANGUAGES, sleep_time=15, mail_connection=None):
+def run_streamer(tw_connection, listener, WORDS, LANGUAGES, sleep_time=15, mail_connection=None, sleep_every_24h=False):
+
+    t1 = time.time()
     while True:
         try:
             # Connect/reconnect the stream
@@ -25,7 +30,14 @@ def run_streamer(tw_connection, listener, WORDS, LANGUAGES, sleep_time=15, mail_
             streamer.filter(track=WORDS, languages=LANGUAGES, async=False)
         except IncompleteRead:
             # Ignore this exception
+            if sleep_every_24h:
+                t1 = sleep_if(streamer, t1)
             continue
+        except AttributeError as e:
+            if sleep_every_24h:
+                t1 = sleep_if(streamer, t1)
+            if bool(re.search('has no attribute', str(e))):
+                continue
         except KeyboardInterrupt:
             # If user interrupts, disconnect stream and break out!
             streamer.disconnect()
@@ -34,6 +46,8 @@ def run_streamer(tw_connection, listener, WORDS, LANGUAGES, sleep_time=15, mail_
             # If it's still the IncompleteRead error
             # that for some reason wasn't captured before
             # continue running the script
+            if sleep_every_24h:
+                t1 = sleep_if(streamer, t1)
             if bool(re.search('IncompleteRead', str(e))):
                 continue
             else:
@@ -44,6 +58,7 @@ def run_streamer(tw_connection, listener, WORDS, LANGUAGES, sleep_time=15, mail_
                         email_me_error("unknown", custom_msg=e_)
                     else:
                         print(e_)
+                    streamer.disconnect()
                     time.sleep(sleep_time)
                     continue
                 except:
@@ -55,3 +70,15 @@ def run_streamer(tw_connection, listener, WORDS, LANGUAGES, sleep_time=15, mail_
                     streamer.disconnect()
                     print(str(e))
                     break
+
+
+def sleep_if(streamer, t1):
+    if (time.time()-t1)//(60*60) > 60*60*24:
+        try:
+            streamer.disconnect()  
+        except:
+            pass
+        print("Will sleep for 48 hours")
+        time.sleep(60*60*48)
+        return time.time()
+
